1cb1c21fc902499b8097c3ecda0ea60a6f4485e9
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-07-01 09:58:13 +0300

    create handler

---
+++ put-message/lambda/main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
+	"net/http"
+	"os"
+	"time"
+)
+
+type Event struct {
+	Queue string `json:"queue"`
+}
+
+type Response struct {
+	Content    string `json:"content"`
+	StatusCode int    `json:"statusCode"`
+}
+
+var (
+	awsRegion          string
+	awsAccessKeyID     string
+	awsSecretAccessKey string
+	awsEndpoint        string
+)
+
+func handler(event Event) (Response, error) {
+	if awsRegion == "" {
+		awsRegion = os.Getenv("AWS_REGION")
+		if awsRegion == "" {
+			panic("got empty AWS_REGION")
+		}
+	}
+	if awsAccessKeyID == "" {
+		awsAccessKeyID = os.Getenv("AWS_ACCESS_KEY_ID")
+		if awsAccessKeyID == "" {
+			panic("got empty AWS_ACCESS_KEY_ID")
+		}
+	}
+	if awsSecretAccessKey == "" {
+		awsSecretAccessKey = os.Getenv("AWS_SECRET_ACCESS_KEY")
+		if awsSecretAccessKey == "" {
+			panic("got empty AWS_SECRET_ACCESS_KEY")
+		}
+	}
+	if awsEndpoint == "" {
+		awsEndpoint = os.Getenv("AWS_ENDPOINT")
+		if awsEndpoint == "" {
+			panic("got empty AWS_ENDPOINT")
+		}
+	}
+
+	config := &aws.Config{
+		Region:      aws.String(awsRegion),
+		Credentials: credentials.NewStaticCredentials(awsAccessKeyID, awsSecretAccessKey, ""),
+	}
+
+	if awsEndpoint != "" {
+		config.Endpoint = aws.String(awsEndpoint)
+	}
+
+	awsSession, err := session.NewSession(config)
+	if err != nil {
+		panic(err)
+	}
+
+	sqsSession := sqs.New(session.Must(awsSession, nil))
+
+	if event.Queue == "" {
+		return response("", http.StatusBadRequest, errors.New("got empty SQS name"))
+	}
+
+	queue := fmt.Sprintf("%s/%s", awsEndpoint, event.Queue)
+
+	err = sendToQueue(sqsSession, queue)
+	if err != nil {
+		return response("unable to put message in queue", http.StatusInternalServerError, err)
+	}
+
+	return response("successfully put message in queue", http.StatusOK, nil)
+}
+
+func main() {
+	lambda.Start(handler)
+}
+
+func sendToQueue(sqsSession *sqs.SQS, queueURL string) error {
+	_, err := sqsSession.SendMessage(&sqs.SendMessageInput{
+		MessageAttributes: map[string]*sqs.MessageAttributeValue{
+			"MyAttr": {
+				DataType:    aws.String("String"),
+				StringValue: aws.String(fmt.Sprintf("Time now is %s", time.Now().Format(time.DateTime))),
+			},
+		},
+		MessageBody: aws.String("Got new event!"),
+		QueueUrl:    aws.String(queueURL),
+	})
+
+	return err
+}
+
+func response(content string, status int, err error) (Response, error) {
+	return Response{
+		Content:    content,
+		StatusCode: status,
+	}, err
+}
