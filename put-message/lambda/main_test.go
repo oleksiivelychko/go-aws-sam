246595185eca4e09528873b995aa87e290c8f273
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-07-14 22:03:44 +0300

    rewrite tests

--- put-message/lambda/main_test.go
+++ put-message/lambda/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/aws/aws-lambda-go/events"
 	"testing"
 )
 
@@ -11,14 +12,17 @@ func TestHandler(t *testing.T) {
 	awsEndpoint = "http://localhost:4566"
 
 	t.Run("Failed request", func(t *testing.T) {
-		resp, err := handler(Event{})
-		if err != nil && err.Error() != "got empty SQS name" {
+		resp, err := handler(events.APIGatewayProxyRequest{})
+		if err != nil && err.Error() != "unexpected end of JSON input" {
 			t.Fatalf("Error failed to trigger with an invalid HTTP response: %v", resp)
 		}
 	})
 
 	t.Run("Successful request", func(t *testing.T) {
-		_, err := handler(Event{Queue: "000000000000/my-queue"})
+		_, err := handler(events.APIGatewayProxyRequest{
+			Body: "{\"queue\":\"000000000000/my-queue\"}",
+		})
+
 		if err != nil {
 			t.Fatalf("Everything should be ok, but %s", err)
 		}
