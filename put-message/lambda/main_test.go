ea516e0bf0bc04391235f9f13711f57b717dc56a
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-07-15 07:26:13 +0300

    rollback tests

--- put-message/lambda/main_test.go
+++ put-message/lambda/main_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/aws/aws-lambda-go/events"
 	"testing"
 )
 
@@ -12,17 +11,14 @@ func TestHandler(t *testing.T) {
 	awsEndpoint = "http://localhost:4566"
 
 	t.Run("Failed request", func(t *testing.T) {
-		resp, err := handler(events.APIGatewayProxyRequest{})
-		if err != nil && err.Error() != "unexpected end of JSON input" {
+		resp, err := handler(Event{})
+		if err != nil && err.Error() != "got empty SQS name" {
 			t.Fatalf("Error failed to trigger with an invalid HTTP response: %v", resp)
 		}
 	})
 
 	t.Run("Successful request", func(t *testing.T) {
-		_, err := handler(events.APIGatewayProxyRequest{
-			Body: "{\"queue\":\"000000000000/my-queue\"}",
-		})
-
+		_, err := handler(Event{Queue: "000000000000/my-queue"})
 		if err != nil {
 			t.Fatalf("Everything should be ok, but %s", err)
 		}
