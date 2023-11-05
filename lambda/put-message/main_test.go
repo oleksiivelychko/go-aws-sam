7e2c2fdd4894c335fe33d15482a0dd1958f684a4
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-11-05 21:27:49 +0200

    edit description

--- lambda/put-message/main_test.go
+++ lambda/put-message/main_test.go
@@ -10,14 +10,14 @@ func TestHandler(t *testing.T) {
 	awsSecretAccessKey = "local"
 	awsEndpoint = "http://localhost:4566"
 
-	t.Run("Failed request", func(t *testing.T) {
+	t.Run("Failed HTTP request", func(t *testing.T) {
 		resp, err := handler(event{})
-		if err != nil && err.Error() != "got empty SQS name" {
-			t.Errorf("got invalid HTTP newResponse: %v", resp)
+		if err != nil && err.Error() != "empty SQS" {
+			t.Errorf("bad response: %v", resp)
 		}
 	})
 
-	t.Run("Successful request", func(t *testing.T) {
+	t.Run("Successful HTTP request", func(t *testing.T) {
 		_, err := handler(event{Queue: "000000000000/MyQueue"})
 		if err != nil {
 			t.Error(err)
