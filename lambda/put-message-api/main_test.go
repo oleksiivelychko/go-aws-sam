27b6a52cbe5f3550f98db0790713479e9ff8f2c0
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-11-05 20:56:56 +0200

    edit AWS_ENDPOINT

--- lambda/put-message-api/main_test.go
+++ lambda/put-message-api/main_test.go
@@ -10,20 +10,17 @@ func TestHandler(t *testing.T) {
 	_ = os.Setenv("AWS_REGION", "us-east-1")
 	_ = os.Setenv("AWS_ACCESS_KEY_ID", "local")
 	_ = os.Setenv("AWS_SECRET_ACCESS_KEY", "local")
-	_ = os.Setenv("AWS_SECRET_ACCESS_KEY", "http://localhost:4566")
+	_ = os.Setenv("AWS_ENDPOINT", "http://localhost:4566")
 
-	t.Run("Failed request", func(t *testing.T) {
+	t.Run("Failed HTTP request", func(t *testing.T) {
 		resp, err := handler(events.APIGatewayProxyRequest{})
 		if err != nil && err.Error() != "unexpected end of JSON input" {
-			t.Errorf("got invalid HTTP response: %v", resp)
+			t.Errorf("bad response: %v", resp)
 		}
 	})
 
-	t.Run("Successful request", func(t *testing.T) {
-		_, err := handler(events.APIGatewayProxyRequest{
-			Body: "{\"queue\":\"000000000000/MyQueue\"}",
-		})
-
+	t.Run("Successful HTTP request", func(t *testing.T) {
+		_, err := handler(events.APIGatewayProxyRequest{Body: "{\"queue\":\"000000000000/MyQueue\"}"})
 		if err != nil {
 			t.Error(err)
 		}
