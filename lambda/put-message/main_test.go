cd14833ecad11f6ce84819d7d1edb5d103031cc5
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-11-11 16:57:22 +0200

    one-line condition

--- lambda/put-message/main_test.go
+++ lambda/put-message/main_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestHandler(t *testing.T) {
 	awsRegion = "us-east-1"
@@ -9,15 +11,13 @@ func TestHandler(t *testing.T) {
 	awsEndpoint = "http://localhost:4566"
 
 	t.Run("Failed HTTP request", func(t *testing.T) {
-		resp, err := handler(event{})
-		if err != nil && err.Error() != "empty SQS" {
+		if resp, err := handler(&event{}); err != nil && err.Error() != "empty SQS" {
 			t.Errorf("bad response: %v", resp)
 		}
 	})
 
 	t.Run("Successful HTTP request", func(t *testing.T) {
-		_, err := handler(event{Queue: "000000000000/MyQueue"})
-		if err != nil {
+		if _, err := handler(&event{Queue: "000000000000/MyQueue"}); err != nil {
 			t.Error(err)
 		}
 	})
